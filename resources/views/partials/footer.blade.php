b3be490fa919a8eecf4d9191b6c23bb65bff3417
Donishjoy <114819415+Donishjoy@users.noreply.github.com>
2024-11-13 07:46:31 +0530

    fix: mailto added

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,62 +3,71 @@
 <footer id="footer" class="footer light-background">
 
     <div class="container footer-top">
-      <div class="row gy-4">
-        <div class="col-lg-5 col-md-12 footer-about">
-          <a href="{{ url('/') }}" class="logo d-flex align-items-center">
-            <span class="sitename">AlKhamisi International Investments</span>
-          </a>
-          <p>Our team includes young, energetic, highly qualified Employees and management professionals. We provide complete services and solutions to help your organization move with the pace of international markets.</p>
-          <div class="social-links d-flex mt-4">
-            <a href=""><i class="bi bi-twitter-x"></i></a>
-            <a href=""><i class="bi bi-facebook"></i></a>
-            <a href=""><i class="bi bi-instagram"></i></a>
-            <a href=""><i class="bi bi-linkedin"></i></a>
-          </div>
+        <div class="row gy-4">
+            <div class="col-lg-5 col-md-12 footer-about">
+                <a href="{{ url('/') }}" class="logo d-flex align-items-center">
+                    <span class="sitename">AlKhamisi International Investments</span>
+                </a>
+                <p>Our team includes young, energetic, highly qualified Employees and management professionals. We
+                    provide complete services and solutions to help your organization move with the pace of
+                    international markets.</p>
+                <div class="social-links d-flex mt-4">
+                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                    <a href=""><i class="bi bi-facebook"></i></a>
+                    <a href=""><i class="bi bi-instagram"></i></a>
+                    <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-6 footer-links">
+                <h4>Useful Links</h4>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/#about">About Us</a></li>
+                    <li><a href="/#services">Services</a></li>
+                    <li><a href="/#contact">Contact Us</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2 col-6 footer-links">
+                <h4>Our Services</h4>
+                <ul>
+                    <li><a href="/construction">Construction</a></li>
+                    <li><a href="/itsupport">IT Services</a></li>
+                    <li><a href="/education">Education Services</a></li>
+                    <li><a href="/foodtrade">Food Trade</a></li>
+                    <li><a href="/tourism">Tourism</a></li>
+                    <li><a href="/realestate">Real Estate</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+                <h4>Contact Us</h4>
+                <p>Al-Khamisi International Investments</p>
+                <p>P.O BOX: 1341</p>
+                <p>P.C: 112</p>
+                <p>CR.NO: 1488445</p>
+                <p>Muscat, Sultanate of Oman</p>
+                <p class="mt-4"><strong>Phone:</strong> <span>+968 92165009</span></p>
+                <p>
+                    <strong>Email:</strong>
+                    <span><a href="mailto:info@alkhamisiintl.com"
+                            style="color: #444444">info@alkhamisiintl.com</a></span>
+                </p>
+                <span style="margin-left: 20%;"><a href="mailto:contact@alkhamisiintl.com"
+                        style="color: #444444">contact@alkhamisiintl.com</a></span>
+                </p>
+            </div>
+
         </div>
-  
-        <div class="col-lg-2 col-6 footer-links">
-          <h4>Useful Links</h4>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/#about">About Us</a></li>
-            <li><a href="/#services">Services</a></li>
-            <li><a href="/#contact">Contact Us</a></li>
-          </ul>
-        </div>
-  
-        <div class="col-lg-2 col-6 footer-links">
-          <h4>Our Services</h4>
-          <ul>
-          <li><a href="/construction">Construction</a></li>
-            <li><a href="/itsupport">IT Services</a></li>
-            <li><a href="/education">Education Services</a></li>
-            <li><a href="/foodtrade">Food Trade</a></li>
-            <li><a href="/tourism">Tourism</a></li>
-            <li><a href="/realestate">Real Estate</a></li>
-          </ul>
-        </div>
-  
-        <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-          <h4>Contact Us</h4>
-          <p>Al-Khamisi International Investments</p>
-          <p>P.O BOX: 1341</p>
-          <p>P.C: 112</p>
-          <p>CR.NO: 1488445</p>
-          <p>Muscat, Sultanate of Oman</p>
-          <p class="mt-4"><strong>Phone:</strong> <span>+968 92165009</span></p>
-          <p><strong>Email:</strong> <span>info@alkhamisiintl.com</span> <span>contact@alkhamisiintl.com</span></p>
-        </div>
-  
-      </div>
     </div>
-  
+
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Al Khamisi</strong> <span>All Rights Reserved</span></p>
-      <div class="credits">
-        {{-- Designed by <a href="">Al Khamisi international investments</a> --}}
-      </div>
+        <p>© <span>Copyright</span> <strong class="px-1 sitename">Al Khamisi</strong> <span>All Rights Reserved</span>
+        </p>
+        <div class="credits">
+            {{-- Designed by <a href="">Al Khamisi international investments</a> --}}
+        </div>
     </div>
-  
-  </footer>
-  
+
+</footer>
